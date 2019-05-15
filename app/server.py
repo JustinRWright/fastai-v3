b56d7b3f3b536d54a403b601882dc7151f73eb9c
JustinRWright <15850245+JustinRWright@users.noreply.github.com>
2019-05-15 18:32:59 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/dicwo8p1mn1daa4/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['chipmunks', 'dogs', 'humans']
